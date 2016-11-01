028632e74e94f3b27b9053648cdfce2fbec59c45
pguzowski <pguzowski@pc2014.hep.manchester.ac.uk>
2016-11-01 14:46:04 +0000

    update script to run in temporary directory

--- flmyptd2root.py
+++ flmyptd2root.py
@@ -2,6 +2,17 @@
 import os
 import argparse
 import subprocess
+from contextlib import contextmanager
+import shutil
+import tempfile
+
+@contextmanager
+def tmpdir():
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
 
 def getConversionScript():
     """ derive path to conversion script from this
@@ -18,15 +29,20 @@ def flbrio2root():
     parser.add_argument("-o", type=str, metavar="<outfile>", required=True, help="output ROOT file")
     args = parser.parse_args()
 
-    try:
-        subprocess.call(["flreconstruct", "-i", args.i, "-p", getConversionScript()])
-        os.rename("testptd.root", args.o)
-    except OSError as e:
-        if e.errno == os.errno.ENOENT:
-            print("error: could not locate flreconstruct program")
-        else:
-            # Something else went wrong while trying to run `flreconstruct`
-            raise
+    fullinpath = os.path.realpath(args.i)
+    fulloutpath = os.path.realpath(args.o)
+
+    with tmpdir() as temp_dir:
+
+        try:
+            subprocess.call(["flreconstruct", "-i", fullinpath, "-p", getConversionScript()],cwd=temp_dir)
+            shutil.move(temp_dir+'/testptd.root', fulloutpath)
+        except OSError as e:
+            if e.errno == os.errno.ENOENT:
+                print("error: could not locate flreconstruct program")
+            else:
+                # Something else went wrong while trying to run `flreconstruct`
+                raise
 
 
 if __name__ == '__main__':
